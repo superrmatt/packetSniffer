d50d6d377103ddda566c03dc90b3dc2d634cd90c
Matt Dambra <mpdambra@gmail.com>
2022-02-23 13:58:21 -0500

    Update packet_sniffer.pl
    Fixing indentations

--- packet_sniffer.pl
+++ packet_sniffer.pl
@@ -51,52 +51,52 @@ while ($i < 10000000){
 	   my $ip = NetPacket::IP->decode($ether_data);
    
 
-	print "Dumper = \n";
-	print Dumper $ip, "\n\n\n";
-	print "-------------------------\n\n\n";
+		print "Dumper = \n";
+		print Dumper $ip, "\n\n\n";
+		print "-------------------------\n\n\n";
 
-	#FYI, there are probably a few more variables I am missing here which are assocaited with a network packet header.
-	my $num;
-	my $ver;
-	my $proto;
-	my $dest_ip;
-	my $chksum;
-	my $src_ip;
-	my $hlen;
-	my $ttl;
-	my $foffset;
-	my $options;
-	my $len;
-	my $tos;
-	my $id;
-	my $flags;
+		#FYI, there are probably a few more variables I am missing here which are assocaited with a network packet header.
+		my $num;
+		my $ver;
+		my $proto;
+		my $dest_ip;
+		my $chksum;
+		my $src_ip;
+		my $hlen;
+		my $ttl;
+		my $foffset;
+		my $options;
+		my $len;
+		my $tos;
+		my $id;
+		my $flags;
 
-	#assign packet header vars to our vars.
-	$ver = $ip->{'ver'};
-	$proto = $ip->{'proto'};
-	$dest_ip =$ip->{'dest_ip'};
-	$chksum = $ip->{'cksum'};
-	$src_ip = $ip->{'src_ip'};
-	$hlen = $ip->{'hlen'};
-	$ttl = $ip->{'ttl'};
-	$foffset = $ip->{'foffset'};
-	$options = $ip->{'options'};
-	$len = $ip->{'len'};
-	$tos = $ip->{'tos'};
-	$id = $ip->{'id'};
-	$flags = $ip->{'flags'};
+		#assign packet header vars to our vars.
+		$ver = $ip->{'ver'};
+		$proto = $ip->{'proto'};
+		$dest_ip =$ip->{'dest_ip'};
+		$chksum = $ip->{'cksum'};
+		$src_ip = $ip->{'src_ip'};
+		$hlen = $ip->{'hlen'};
+		$ttl = $ip->{'ttl'};
+		$foffset = $ip->{'foffset'};
+		$options = $ip->{'options'};
+		$len = $ip->{'len'};
+		$tos = $ip->{'tos'};
+		$id = $ip->{'id'};
+		$flags = $ip->{'flags'};
    
-	my $connection;
-	my $query;
-	my $result;
+		my $connection;
+		my $query;
+		my $result;
 
-	#open connection to a DB and insert all of the values. Make sure the DB mirrors these values, or change them as you see fit.
-	$connection = DBI->connect(<DBNAME example to right> -> "DBI:mysql:<name>:127.0.0.1", <username>, <password>);
-	$query = $connection->prepare("insert into packets values ($i, $ver, $proto, '$dest_ip', $chksum, '$src_ip', $hlen, $ttl, $foffset, '$options', $len, $tos, $id, $flags);");
-	$result = $query->execute();
-   
+		#open connection to a DB and insert all of the values. Make sure the DB mirrors these values, or change them as you see fit.
+		$connection = DBI->connect(<DBNAME example to right> -> "DBI:mysql:<name>:127.0.0.1", <username>, <password>);
+		$query = $connection->prepare("insert into packets values ($i, $ver, $proto, '$dest_ip', $chksum, '$src_ip', $hlen, $ttl, $foffset, '$options', $len, $tos, $id, $flags);");
+		$result = $query->execute();
+  	 
 
-	$i = $i + 1;
+		$i = $i + 1;
 	}
 }
 
